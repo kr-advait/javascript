45ee928ebd5cc419aaaadcfa62a46d6a883d5c42
kr-advait <advait.t@knackroot.com>
2023-01-31 18:53:42 +0530

    Learning asynchronous javascript

--- Async/promises.js
+++ Async/promises.js
@@ -3,29 +3,156 @@
 const fruits = ['apple','banana','chikoo','pear','avocado','watermelon'];
 
 const fruitDish = new Promise((resolve,reject)=>{
-    if(!fruits.includes('apple')){
-        resolve("fruit is present");
+    if(fruits.includes('apple')){
+        resolve("[fruit is present]");
     }
     else{
-        // reject("fruit is not present");
-        throw new Error("not present");
+        reject("fruit is not present");
+        // throw new Error("not present");
     }
 });
 
 fruitDish.then(
-    (value)=>{console.log(`${value} 1`);},
+    (value)=>{console.log(`${value} - 1st deg 1st prom`);},
     (error)=>{console.log(error);}
 );
-
-// or
-
+    
+    // or
+    
 fruitDish
 .then((value)=>{
-    console.log(`[${value}] 2 before return in 1st .then()`);
-    return value+' new';
+    console.log(`${value} - 1st deg 2nd prom`);
+    return value;
 })
 .then((value)=>{
-    console.log(`[${value}] 2 after return in 2nd .then()`);
+    console.log(`${value} - 2nd deg 2nd prom`);
+    return value;
+})
+.then((value)=>{
+    console.log(`${value} - 3rd deg 2nd prom`);
+})
+.catch((error)=>{
+    console.log(error);
+})
+
+
+// function returning promise
+
+function checkFruit(){
+    const fruits = ['apple','banana','chikoo','pear','avocado','watermelon'];
+    
+    return new Promise((resolve,reject)=>{
+        if(fruits.includes('apple')){
+            resolve("[fruit is present]");
+        }
+        else{
+            reject("fruit is not present");
+        }
+    });
+    
+}
+
+
+checkFruit()
+.then((value)=>{
+    console.log(`${value} - 1st deg 3rd prom`);
+    return value;
+})
+.then((value)=>{
+    console.log(value,"- 2nd deg 3rd prom");
+})
+.catch((error)=>{
+    console.log(error);
+})
+
+
+
+
+
+
+// promise resolve
+
+const newPromise = Promise.resolve(5);
+
+newPromise
+.then((value)=>{
+    console.log(value);
+});
+
+
+
+
+const heading1 = document.getElementById('heading1');
+const heading2 = document.getElementById('heading2');
+const heading3 = document.getElementById('heading3');
+const heading4 = document.getElementById('heading4');
+const heading5 = document.getElementById('heading5');
+const heading6 = document.getElementById('heading6');
+const heading7 = document.getElementById('heading7');
+const heading8 = document.getElementById('heading8');
+const heading9 = document.getElementById('heading9');
+const heading10 = document.getElementById('heading10');
+
+
+function changeText(element,text,color,timeInMs){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            if(element){
+                let originalText = element.textContent;
+                element.textContent = text;
+                element.style.color = color;
+                console.log(`resolved for ${originalText}`);
+               resolve(`${originalText} is now ${text} and it's color is ${color}`);
+            }
+            else{
+                reject("promise rejected");
+            }
+        },timeInMs);
+    })
+}
+
+
+// promise chain 
+
+changeText(heading1,'one','tomato',1000)
+.then((value)=>{
+    console.log(value)
+    return changeText(heading2,'two','teal',1000);
+})
+.then((value)=>{
+    console.log(value)
+    return changeText(heading3,'three','purple',1000);
+})
+.then((value)=>{
+    console.log(value);
+    return changeText(heading4,'four','cyan',1000);
+})
+.then((value)=>{
+    console.log(value);
+    return changeText(heading5,'five','red',1000);
+})
+.then((value)=>{
+    console.log(value);
+    return changeText(heading6,'six','blue',1000);
+})
+.then((value)=>{
+    console.log(value);
+    return changeText(heading7,'seven','brown',1000);
+})
+.then((value)=>{
+    console.log(value);
+    return changeText(heading8,'eight','green',1000);
+})
+.then((value)=>{
+    console.log(value);
+    return changeText(heading9,'nine','violet',1000);
+})
+.then((value)=>{
+    console.log(value);
+    return changeText(heading10,'ten','orange',1000);
+})
+.then((value)=>{
+    console.log(value);
 })
 .catch((error)=>{
     console.log(error);
--- OOP/oops.js
+++ OOP/oops.js
@@ -41,3 +41,52 @@ user.publicMethod();
 console.log(user)
 
 
+
+
+// call, apply, bind
+
+function giveInfo(laptop,technology,funcName){
+    return(`${funcName} - Hello, ${this.name}. Your age is ${this.age}. You have ${laptop} laptop and working on ${technology}.`);
+}
+
+const userAdvait = {
+    name : "advait",
+    age : 20
+}
+
+const userKaran = {
+    name : "karan",
+    age : 21
+}
+
+
+
+// call  -   functionName.call(objectName,param1,param2,....)
+
+let advaitInfo = giveInfo.call(userAdvait,"Dell","Backend","Call");
+console.log(advaitInfo);
+
+let karanInfo = giveInfo.call(userKaran,"Dell","Frontend","Call");
+console.log(karanInfo);
+
+
+
+
+// apply    -   functionName.apply(objectName,[param1,param2,....])
+
+let advaitInfo2 = giveInfo.apply(userAdvait,["Dell","Backend","Apply"]);
+console.log(advaitInfo2);
+
+let karanInfo2 = giveInfo.apply(userKaran,["Dell","Frontend","Apply"]);
+console.log(karanInfo2);
+
+
+
+
+// bind    -   functionName.bind(objectName,param1,param2,....)
+
+let showAdvaitInfo = giveInfo.bind(userAdvait,"Dell","Backend","Bind");
+console.log(showAdvaitInfo());
+
+let showKaranInfo = giveInfo.bind(userKaran);
+console.log(showKaranInfo("Dell","Frontend","Bind"));
